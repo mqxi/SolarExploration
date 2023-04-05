9f696e8c658ee121075933904ee874e7093b35ba
Maxi <englemax@dhbw-loerrach.de>
2023-04-05 21:14:50 +0200

    inital

---
+++ app/app.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("https://github.com/mqxi/SolarExploration/blob/de2e9957fbf05c5adfa34519b19c036ac10bea76/data/Solar_Energy_Production.csv")
+print(df)
