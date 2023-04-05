36f88cf4f08d414f4ecc00e23432627e9e086c72
Maxi <englemax@dhbw-loerrach.de>
2023-04-05 21:18:55 +0200

    inital

--- app/app.py
+++ app/app.py
@@ -2,6 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import requests
 
-df = pd.read_csv("https://github.com/mqxi/SolarExploration/blob/de2e9957fbf05c5adfa34519b19c036ac10bea76/data/Solar_Energy_Production.csv")
-print(df)
+url = "https://raw.githubusercontent.com/mqxi/SolarExploration/9f696e8c658ee121075933904ee874e7093b35ba/data/Solar_Energy_Production.csv"
+res = requests.get(url, allow_redirects=True)
+with open("Solar_Energy_Production.csv", "wb") as file:
+    file.write(res.content)
+solar_energy = pd.read_csv("Solar_Energy_Production.csv")
+print(solar_energy)
